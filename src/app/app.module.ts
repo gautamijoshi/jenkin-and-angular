0a6fca108bfa6c491a7df8dcd3716c461a566ae6
gautami <gjoshi2104@gmail.com>
2019-11-28 00:55:16 +0530

    jaude

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RegisterComponent } from './register/register.component';
+
+
+import { RouterModule } from '@angular/router';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   	
+    RegisterComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    RouterModule.forRoot([   {path:"home",component:RegisterComponent},
+    
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
